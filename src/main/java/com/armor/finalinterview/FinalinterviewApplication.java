4b9abb83401a6da3d4f458d6ba4d7c2ddec79371
Joyce Ling <yueh.j.ling@gmail.com>
2018-02-13 10:17:06 -0600

    Initial commit

---
+++ src/main/java/com/armor/finalinterview/FinalinterviewApplication.java
@@ -0,0 +1,12 @@
+package com.armor.finalinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalinterviewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/armor/finalinterview/FinalinterviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.armor.finalinterview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalinterviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
