6aa7b0c47c0ff93387f3d87c21885f42fe1c05d9
Joyce Ling <yueh.j.ling@gmail.com>
2018-02-13 12:15:08 -0600

    * basic button formatting * add landing page

--- src/main/java/com/armor/finalinterview/controllers/SupportController.java
+++ src/main/java/com/armor/finalinterview/controllers/SupportController.java
@@ -2,14 +2,28 @@ package com.armor.finalinterview.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SupportController {
 
-    @GetMapping("/")
-    public String supportLanding () {
 
-        return "index";
+    @GetMapping("/")
+    public String landing () {
+        return "landing";
+    }
+
+    @GetMapping("/support")
+    public String getSupportForm () {
+
+        return "support-form";
+    }
+
+    @PostMapping("/support")
+    public String postSupportForm () {
+
+        return "support-form";
     }
 
 }
